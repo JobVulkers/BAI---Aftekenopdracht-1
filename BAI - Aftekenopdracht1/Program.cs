4f27ab002a7e7f44f33892e93344ef0821c681f4
JobVulkers <55084678+JobVulkers@users.noreply.github.com>
2020-10-08 10:20:35 +0200

    Final Version

--- BAI - Aftekenopdracht1/Program.cs
+++ BAI - Aftekenopdracht1/Program.cs
@@ -112,8 +112,7 @@ namespace BAI
             int i = 0;
             while (i < count)
             {
-                int getal = random.Next(lower, upper);
-                           
+                int getal = random.Next(lower, upper + 1 );
                 //hier
                 if (!D1.ContainsValue(getal))
                 {
@@ -121,10 +120,8 @@ namespace BAI
                     stack.Push(getal);
                     i++;
                 }
-
             }
-                return stack;
-            
+                return stack;            
         }
 
 
@@ -148,29 +145,27 @@ namespace BAI
             {
                 List<int> list;
                 Queue<int> queue;
-                Stack<int> stack;
+                Stack <int> stack;
 
-                Console.WriteLine();
                 Console.WriteLine("=== Opdracht 1 : FilterList ===");
                 list = new List<int>() { 1, 3, 5, 7, 3, 8, 9, 5 };
                 /* PrintEnumerable(list);*/
                 Opdr1FilterList(list);
                 PrintEnumerable(list);
 
-                Console.WriteLine();
-                Console.WriteLine("=== Opdracht 2 : Stack / Queue ===");
+                Console.WriteLine("\n=== Opdracht 2 : Stack / Queue ===");
                 queue = Opdr2aQueue50();
                 PrintEnumerable(queue);
                 stack = Opdr2bStackFromQueue(queue);
                 PrintEnumerable(stack);
 
-                Console.WriteLine();
-                Console.WriteLine("=== Opdracht 3 : Random number ===");
+                Console.WriteLine("\n=== Opdracht 3 : Random number ===");
                 stack = Opdr3RandomNumbers(100, 150, 10);
                 PrintEnumerable(stack);
                 stack = Opdr3RandomNumbers(10, 15, 6);
                 PrintEnumerable(stack);
                 stack = Opdr3RandomNumbers(10_000, 50_000, 40_001);
+                PrintEnumerable(stack);
             }
         }
     }
